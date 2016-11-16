a5f7bcea6d7e1bbd60a6b637a8fb159a6c014fb3
Igor Dvorkin <idvorkin@gmail.com>
2016-11-16 11:30:32 -0800

    More info on init, first cut of compensation.

---
+++ _posts/2016-11-16-understandingtechcompensation.markdown
@@ -0,0 +1,25 @@
+---
+layout: post
+title: "UnderstandingTechCompensation"
+date: "2016-11-16 07:00:35 Pacific Standard Time"
+---
+
+Understanding total compensation and signing bonuses
+
+People get excited when they get a large signing bonus, but often all that's happening is they are covering the time till bonus. 
+
+When comparing compensation between companies, you want to compare total compensation - which breaks down into salary, cash bonus, and equity.  As an example, imagine making a total compensation of 200$ per year(1):
+
+* Base salary: 150$/year, paid out bi-weekly. 
+* Cash Bonus: 25$/year,  paid out weeky
+* Equity Grant: 25$/year, paid out anually vesting over 5 years. 
+* Equity 5th company: 25$ paid out annually.
+* Equity 1st year: 5$ paid out first year. 
+
+Now, lets talk about signing bonus. People often get excited that they get a huge signing bonus, but lets do some math. Ia
+
+---
+* Unvested Stock
+
+Notes 
+(1)  Bonus and equity are usually variable based on performance, but for this article, lets assume they are fixed.
--- init.bat
+++ init.bat
@@ -1,10 +1,36 @@
 REM From - https://tibel.github.io/posts/jekyll-3.0.x-portable/ 
 REM 1st time follow directions to setup ruby into \jekyll
-REM gem install activesupport github-pages wdm
 REM
 @ECHO OFF
+
+
 ECHO Adding Jekyll to PATH...
 SET RUBY_PATH=c:\jekyll\ruby
 SET DEVKIT_PATH=c:\jekyll\devkit
 SET PATH=%RUBY_PATH%\bin;%DEVKIT_PATH%\bin;%DEVKIT_PATH%\mingw\bin;%PATH%
+goto :eof
+
+REM First time setup
+:first_time_setup
+goto :eof
+REM GRAH WHAT A MESS.
+REM  Just use the binaries @ OneDrive\jekyll.bat
+REM GRAH WHAT A MESS.
+wget -nc http://dl.bintray.com/oneclick/rubyinstaller/ruby-2.1.7-i386-mingw32.7z
+echo Write dekvit to \jekyll\ruby-blah
+wget -nc http://dl.bintray.com/oneclick/rubyinstaller/DevKit-mingw64-32-4.7.2-20130224-1151-sfx.exe
+echo Write dekvit to \jekyll\devkit
+
+wget -nc https://raw.githubusercontent.com/rubygems/rubygems/master/lib/rubygems/ssl_certs/rubygems.org/AddTrustExternalCARoot.pem
+
+echo copy into old cert location.
+#git sources --remove https://rubygems.org
+#git sources -a https://rubygems.org
+copy AddTrustExternalCARoot.pem C:\jekyll\ruby\lib\ruby\2.1.0\rubygems\ssl_certs
+
+gem install activesupport github-pages wdm jekyll
+
+
+:goto eof
+
 
