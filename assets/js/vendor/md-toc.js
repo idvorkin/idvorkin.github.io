ba1677b19e5133eb26c9346d4b6774eb06432a8a
Igor Dvorkin <idvorkin@gmail.com>
2020-03-15 02:35:16 +0000

    Merge (prettier formatted) changes from: hackmdio/md-toc
    https://github.com/hackmdio/codimd/blob/develop/public/vendor/md-toc.js

--- assets/js/vendor/md-toc.js
+++ assets/js/vendor/md-toc.js
@@ -1,3 +1,4 @@
+/* eslint-env browser, jquery */
 /**
  * md-toc.js v1.0.2
  * https://github.com/yijian166/md-toc.js
@@ -10,8 +11,10 @@
     this.options = options || {};
     this.tocLevel = parseInt(options.level) || 0;
     this.tocClass = options["class"] || "toc";
+    this.ulClass = options["ulClass"];
     this.tocTop = parseInt(options.top) || 0;
     this.elChilds = this.el.children;
+    this.process = options["process"];
     if (!this.elChilds.length) return;
     this._init();
   }
@@ -23,8 +26,9 @@
   };
 
   Toc.prototype._collectTitleElements = function() {
-    (this._elTitlesNames = []), (this.elTitleElements = []);
-    for (var i = 1; i < 7; i++) {
+    this._elTitlesNames = [];
+    this.elTitleElements = [];
+    for (var i = 1; i < 6; i++) {
       if (this.el.getElementsByTagName("h" + i).length) {
         this._elTitlesNames.push("h" + i);
       }
@@ -43,64 +47,104 @@
     }
   };
 
-  Toc.prototype._createTocContent = function() {
-    this._elTitleElementsLen = this.elTitleElements.length;
-    if (!this._elTitleElementsLen) return;
-    this.tocContent = "";
-    this._tempLists = [];
+  Toc.prototype._createTocContent = function recursiveToc(
+    level = 0,
+    titleElements = [],
+    titleNames = [],
+    ulClass = undefined,
+    index = 0
+  ) {
+    // Inititalize our elements from the toc object
+    // which is only available on level 0
+    if (level === 0) {
+      titleElements = this.elTitleElements;
+      titleNames = this._elTitlesNames;
+      ulClass = this.ulClass;
+    }
+    // No need to do anything for an empty ToC
+    if (!titleElements.length) return;
 
-    // 本页面的完整地址，某些情况下base标签和页面地址不一致，会造成锚点混乱
-    var url = location.origin + location.pathname;
-    for (var i = 0; i < this._elTitleElementsLen; i++) {
-      var j = i + 1;
-      this._elTitleElement = this.elTitleElements[i];
-      this._elTitleElementName = this._elTitleElement.tagName;
-      this._elTitleElementText = this._elTitleElement.innerHTML;
-      this._elTitleElement.setAttribute("id", "tip" + i);
+    var content = "<ul";
+    if (ulClass) {
+      content += ' class="' + ulClass + '"';
+    }
+    content += ">\n";
+    var iterTag = titleNames[level];
+    var recurse = false;
+    var openTag = false;
 
-      this.tocContent +=
-        '<li><a href="' +
-        url +
-        "#tip" +
-        i +
-        '">' +
-        this._elTitleElementText +
-        "</a>";
+    for (var element; (element = titleElements.shift()); ) {
+      var elementTag = element.tagName.toLowerCase();
 
-      if (j != this._elTitleElementsLen) {
-        this._elNextTitleElementName = this.elTitleElements[j].tagName;
-        if (this._elTitleElementName != this._elNextTitleElementName) {
-          var checkColse = false,
-            y = 1;
-          for (var t = this._tempLists.length - 1; t >= 0; t--) {
-            if (this._tempLists[t].tagName == this._elNextTitleElementName) {
-              checkColse = true;
-              break;
-            }
-            y++;
-          }
-
-          if (checkColse) {
-            this.tocContent += new Array(y + 1).join("</li></ul>");
-            this._tempLists.length = this._tempLists.length - y; //更新栈的长度。
-          } else {
-            this._tempLists.push(this._elTitleElement);
-            this.tocContent += "<ul>";
-          }
+      // We only care about tags on our level to add them as list item
+      if (elementTag == iterTag) {
+        // Let's do some cleaning
+        var elementTitle = element.textContent.replace(/"/g, "&quot;");
+        var elementText = (typeof this.process === "function"
+          ? this.process(element)
+          : element.innerHTML
+        ).replace(/<(?:.|\n)*?>/gm, "");
+        var id = element.getAttribute("id");
+        if (!id) {
+          element.setAttribute("id", "tip" + ++index);
+          id = "#tip" + index;
         } else {
-          this.tocContent += "</li>";
+          id = "#" + id;
         }
+        if (openTag) {
+          content += "</li>\n";
+          openTag = false;
+        }
+        content +=
+          '<li><a href="' +
+          id +
+          '" title="' +
+          elementTitle +
+          '">' +
+          elementText +
+          "</a>";
+        // Reset recursion. We need it for the next subsections
+        recurse = false;
+        openTag = true;
+        // Check if the current element has a lower level than ours, if so, we have to go down the rabbithole!
+      } else if (
+        !recurse &&
+        titleNames.indexOf(elementTag.toLowerCase()) > level
+      ) {
+        recurse = true;
+        if (!openTag) {
+          content += '<li class="invisable-node">';
+          openTag = true;
+        }
+        // This element is for the lower lever, we have to re-add it before we send the list down there.
+        titleElements.unshift(element);
+        // Let's call ourself and get to the next level
+        content += recursiveToc(
+          level + 1,
+          titleElements,
+          titleNames,
+          ulClass,
+          index
+        );
       } else {
-        if (this._tempLists.length) {
-          this.tocContent += new Array(this._tempLists.length + 1).join(
-            "</li></ul>"
-          );
-        } else {
-          this.tocContent += "</li>";
-        }
+        // When we end up here, met a higher level element
+        // This is not our business so back into the list with the element and let's end this loop
+        titleElements.unshift(element);
+        break;
       }
     }
-    this.tocContent = "<ul>" + this.tocContent + "</ul>";
+
+    if (openTag) {
+      content += "</li>\n";
+    }
+    content += "</ul>\n";
+
+    // Set ToC content of the level 0 everything else pass things to the upper level!
+    if (level === 0) {
+      this.tocContent = content;
+    } else {
+      return content;
+    }
   };
 
   Toc.prototype._showToc = function() {
@@ -108,15 +152,11 @@
     this.toc.innerHTML = this.tocContent;
     this.toc.setAttribute("class", this.tocClass);
     if (!this.options.targetId) {
-      // 没有传入目标id，追加到生成目录的div内
       this.el.appendChild(this.toc);
     } else {
-      //有传入目标id，直接在目标id内生成div
       document.getElementById(this.options.targetId).appendChild(this.toc);
     }
-
     var self = this;
-
     if (this.tocTop > -1) {
       window.onscroll = function() {
         var t = document.documentElement.scrollTop || document.body.scrollTop;
@@ -131,6 +171,5 @@
       };
     }
   };
-
   window.Toc = Toc;
 })(window);
