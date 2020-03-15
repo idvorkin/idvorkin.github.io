ba7beaaddc34ff110d29a625ea6448faaa817a20
Igor Dvorkin <idvorkin@gmail.com>
2020-03-15 04:07:44 +0000

    Add more TOC goop

---
+++ assets/js/vendor/copied_from_hackmd_extras.js
@@ -0,0 +1,64 @@
+let tocExpand = false;
+
+function checkExpandToggle() {
+  const toc = $(".ui-toc-dropdown .toc");
+  const toggle = $(".expand-toggle");
+  if (!tocExpand) {
+    toc.removeClass("expand");
+    toggle.text("Expand all");
+  } else {
+    toc.addClass("expand");
+    toggle.text("Collapse all");
+  }
+}
+
+// toc
+export function generateToc(id) {
+  const target = $(`#${id}`);
+  target.html("");
+  /* eslint-disable no-unused-vars */
+  var toc = new window.Toc("doc", {
+    level: 3,
+    top: -1,
+    class: "toc",
+    ulClass: "nav",
+    targetId: id
+    //    process: getHeaderContent
+  });
+  /* eslint-enable no-unused-vars */
+  if (target.text() === "undefined") {
+    target.html("");
+  }
+  const tocMenu = $('<div class="toc-menu"></div');
+  const toggle = $('<a class="expand-toggle" href="#">Expand all</a>');
+  const backtotop = $('<a class="back-to-top" href="#">Back to top</a>');
+  const gotobottom = $('<a class="go-to-bottom" href="#">Go to bottom</a>');
+  checkExpandToggle();
+  toggle.click(e => {
+    e.preventDefault();
+    e.stopPropagation();
+    tocExpand = !tocExpand;
+    checkExpandToggle();
+  });
+  backtotop.click(e => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (window.scrollToTop) {
+      window.scrollToTop();
+    }
+    removeHash();
+  });
+  gotobottom.click(e => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (window.scrollToBottom) {
+      window.scrollToBottom();
+    }
+    removeHash();
+  });
+  tocMenu
+    .append(toggle)
+    .append(backtotop)
+    .append(gotobottom);
+  target.append(tocMenu);
+}
