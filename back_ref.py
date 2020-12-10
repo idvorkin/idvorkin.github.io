8f29112688ff1d336998fa3880401855c3271a8c
Igor Dvorkin <idvorkin@gmail.com>
2020-12-10 16:36:24 +0000

    Save back_ref.py

--- back_ref.py
+++ back_ref.py
@@ -4,19 +4,19 @@ import os
 
 
 class RefBuilder:
-    def allow_dest_ref(self,r):
-        deny_list = "404.html;all.html;toc.html;index.html".split(';')
+    def allow_dest_ref(self, r):
+        deny_list = "404.html;all.html;toc.html;index.html".split(";")
         for deny in deny_list:
             if r.endswith(deny):
                 return False
         return True
 
     def allow_src_ref(self, r):
-        if r == '/':
+        if r == "/":
             return False
-        if r.startswith('/tags#'):
+        if r.startswith("/tags#"):
             return False
-        return r.startswith('/')
+        return r.startswith("/")
 
     def get_refs(self, path):
         refs = []
@@ -31,7 +31,7 @@ class RefBuilder:
                 self.redirects[path] = "find_link"
             # <title>Redirecting&hellip;</title>
             # <link rel="canonical" href="http://localhost:4000/happy">
-            #if isredirect(soup):
+            # if isredirect(soup):
             #    self.redirect[path] = redirect
         return refs
 
@@ -51,38 +51,38 @@ class RefBuilder:
         for path_src in self.refs.keys():
             self.refs[path_src] = list(set(self.refs[path_src]))
 
-
     def __init__(self):
-        self.refs = defaultdict(list) # src -> dest ; 'root' -> back_links
+        self.refs = defaultdict(list)  # src -> dest ; 'root' -> back_links
         # build this at the same time.
-        self.redirects = {} # redirect->root page
+        self.redirects = {}  # redirect->root page
 
     def dump(self):
         self.dedup()
         for path_src in self.refs.keys():
             print(f"{path_src}:")
             print(f"->{self.refs[path_src]}")
-        print ("Redirects")
+        print("Redirects")
         print(self.redirects)
-
 
 
 def appendXRefToFile(refreneces, output_file):
     pass
 
-def build_refs_for_dir(rb,dir):
+
+def build_refs_for_dir(rb, dir):
     for f in os.listdir(dir):
         if not f.endswith(".html"):
             continue
         rb.gather_refs(f"{dir}/{f}")
 
+
 def main():
     print("Main")
     rb = RefBuilder()
-    dirs = "_site;_site/td;_site/d".split(';')
+    dirs = "_site;_site/td;_site/d".split(";")
     for d in dirs:
-         build_refs_for_dir(rb,d)
+        build_refs_for_dir(rb, d)
     rb.dump()
 
-main()
 
+main()
