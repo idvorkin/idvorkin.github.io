19debe32c4f79f875563f9528feec4f9faa502f8
Igor Dvorkin <idvorkin@gmail.com>
2021-02-28 08:57:07 -0800

    Adding a load random page on top script

--- _ig66/index.md
+++ _ig66/index.md
@@ -1,12 +1,17 @@
 ---
 title: Personal Journal
 layout: post
+
 no-render-title: true
 ---
 
-The days are long, but the months fly by. That's what a lady told us at the playground back when Zach was 1. That stuck with me and I've been trying to document our family adventures ever since. You can find the previoius posts [here](https://ig66.blogspot.com) until I get them migrated.
+The days are long, but the months fly by. That's what a lady told us at the playground back when Zach was 1. That stuck with me and I've been trying to document our family adventures ever since.
 
 <br/>
+
+<div class='alert alert-success' id="random-post">
+A blast from the past!
+</div>
 
 {% for item in site.ig66 reversed   %}
 
--- assets/js/blogger_import.js
+++ assets/js/blogger_import.js
@@ -1,39 +1,41 @@
+function append_post(div, post) {
+  let item = $("<div/>");
+  // TODO: HACK: Strip to the right of Week number
+  let title_href = `<h3> <a href='${post.url}'}>${post.title}</a></h3>`;
+
+  item.append(title_href);
+  // HACK: Upsize to larger thumbnail, seems like blogger auto resizes to include a 320 image as well.
+  let thumbnail_url = post.thumbnail.replace("s72-c", "s320");
+  if (post.thumbnail != "") {
+    item.append(
+      `<div> <a href='${post.url}'}><img src='${thumbnail_url}'/></div>`
+    );
+  }
+  div.append(item);
+}
 function ProcessImports(posts) {
   if (!posts) {
     console.log(`No posts being imported`);
     return;
   }
 
-  let post_location = $("#imported-posts");
-  if (!post_location) {
-    console.log("No back_link_location");
+  //
+  // Import all history
+  let random_div = $("#random-post");
+  let import_div = $("#imported-posts");
+  if (!import_div) {
+    console.log("import-div not found");
     return;
   }
-
-  console.log("mark");
-
-  post_location.append("<h2 id='imported-post'> <b>IMPORTED_NOTES</b><h2>");
-
-  /*
-    posts
-    var sort_descending_by_size = (a, b) =>
-        Number(bl_ui[b].doc_size) - Number(bl_ui[a].doc_size);
-*/
+  // Add a random post on top
+  var randomPost = posts[Math.floor(Math.random() * posts.length)];
+  append_post(random_div, randomPost);
 
   for (var post of posts) {
-    console.log(post.title);
-    console.log(post.url);
-    title_href = `<h3> <a href='${post.url}'}>${post.title}</a></h3>`;
-    post_location.append(title_href);
-    // HACK: Upsize to larger thumbnail, seems like blogger auto resizes to include a 320 image as well.
-    var thumbnail_url = post.thumbnail.replace("s72-c", "s320");
-    if (post.thumbnail != "") {
-      post_location.append(
-        `<div> <a href='${post.url}'}><img src='${thumbnail_url}'/></div>`
-      );
-    }
+    append_post(import_div, post);
   }
 }
+
 function add_imported_blog_posts() {
   var imported_posts_url = "/ig66/ig66-export.json";
   $.getJSON(imported_posts_url, ProcessImports);
