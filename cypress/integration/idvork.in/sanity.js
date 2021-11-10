a6cc70156435ca31131a5caf44801f1b637f5c79
Igor Dvorkin <idvorkin@gmail.com>
2021-11-10 05:40:36 -0800

    Adding cypress for e2ee tests - kind of cool.

---
+++ cypress/integration/idvork.in/sanity.js
@@ -0,0 +1,43 @@
+/// <reference types="cypress" />
+
+// Welcome to Cypress!
+//
+// This spec file contains a variety of sample tests
+// for a todo list app that are designed to demonstrate
+// the power of writing tests in Cypress.
+//
+// To learn more about how Cypress works and
+// what makes it such an awesome testing tool,
+// please read our getting started guide:
+// https://on.cypress.io/introduction-to-cypress
+
+describe('example to-do app', () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit('http://localhost:4000')
+  })
+
+
+  ,it('can load', () => {
+
+      // assert loaded
+
+  })
+  , it('go to ig66', () => {
+    cy.contains("Personal Diary").click()
+    cy.location('pathname').should('match', /\/ig66/);
+
+      // assert loaded
+
+  }),
+  it('go to techdiary', () => {
+
+        cy.contains("Tech Diary").click()
+    cy.location('pathname').should('match', /\/td/);
+      // assert loaded
+
+  })
+})
