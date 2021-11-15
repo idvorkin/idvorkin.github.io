58f0c62e9ac3d6410a7133c225cb427a629a558c
Igor Dvorkin <idvorkin@gmail.com>
2021-11-15 05:52:57 -0800

    Generating assets via TSC

---
+++ assets/js/live-photo-image.js
@@ -0,0 +1,27 @@
+"use strict";
+var LivePhotoImagePrototype = Object.create(HTMLImageElement.prototype);
+LivePhotoImagePrototype.start = function(evt) {
+  if (this.src && !this.originalSrc) {
+    this.originalSrc = this.src;
+    this.src = this.src.replace(/\.[^\.]+$/, ".gif");
+    evt.preventDefault();
+  }
+};
+LivePhotoImagePrototype.stop = function(evt) {
+  if (this.originalSrc) {
+    this.src = this.originalSrc;
+    this.originalSrc = null;
+    evt.preventDefault();
+  }
+};
+LivePhotoImagePrototype.createdCallback = function() {
+  this.addEventListener("mouseenter", this.start);
+  this.addEventListener("mouseleave", this.stop);
+  this.addEventListener("touchstart", this.start);
+  this.addEventListener("touchcancel", this.stop);
+  this.addEventListener("touchend", this.stop);
+};
+var LivePhotoImage = document.registerElement("live-photo-image", {
+  prototype: LivePhotoImagePrototype,
+  extends: "img"
+});
