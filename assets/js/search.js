71f17c543d400349acc9387183aae43dd1651469
Igor Dvorkin <idvorkin@gmail.com>
2021-11-24 12:02:41 -0800

    Updating search to latest algolia

---
+++ assets/js/search.js
@@ -0,0 +1,68 @@
+// Adding a query paramater.
+// import { algoliasearch,instantsearch } from "algoliasearch";
+function getParameterByName(name, url) {
+    if (!url)
+        url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"), results = regex.exec(url);
+    if (!results)
+        return null;
+    if (!results[2])
+        return "";
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+function hitTemplate(hit) {
+    // https://www.algolia.com/doc/api-reference/widgets/infinite-hits/js/
+    let string_rep = "";
+    try {
+        let url = "";
+        if (hit.anchor) {
+            url = `${hit.url}#${hit.anchor}`;
+        }
+        else {
+            url = `${hit.url}`;
+        }
+        if (!hit._highlightResult) {
+            console.log("No Highlight", hit);
+        }
+        const title = hit._highlightResult.title.value;
+        const content = hit._highlightResult.content.value;
+        string_rep = `
+            <h3><a href="${url}">${title}</a></h3>
+            <section class="notepad-post-excerpt"><p>${content}</p></section>
+        `;
+    }
+    catch (err) {
+        console.log("Error in hitTemplate", err, hit);
+    }
+    return string_rep;
+}
+;
+function create_search(appid, search_api_key, index_name, initial_query) {
+    // Instanciating InstantSearch.js with Algolia credentials
+    const search = instantsearch({
+        searchClient: algoliasearch(appid, search_api_key),
+        indexName: index_name,
+        searchParameters: {
+            query: initial_query,
+        },
+    });
+    // Adding searchbar and results widgets
+    search.addWidget(instantsearch.widgets.searchBox({
+        container: "#search-box",
+        placeholder: "Search Igor's musings...",
+        poweredBy: true,
+        showSubmit: false,
+        showReset: false,
+        showLoadingIndicator: false,
+    }));
+    search.addWidget(instantsearch.widgets.infiniteHits({
+        container: "#search-hits",
+        templates: {
+            item: hitTemplate,
+        },
+    }));
+    return search;
+}
+export { create_search, getParameterByName };
+//# sourceMappingURL=search.js.map
