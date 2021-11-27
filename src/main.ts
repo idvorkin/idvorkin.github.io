d1a94a5e82bbbae48e82adaa9d26dbb9738ac5a6
Igor Dvorkin <idvorkin@gmail.com>
2021-11-26 21:37:03 -0800

    Save main.ts

--- src/main.ts
+++ src/main.ts
@@ -282,7 +282,6 @@ function random_prompt_loader() {
 }
 
 function on_monkey_button_click(e) {
-  console.log("MOnkey Button Hit");
   if (window.location.href.includes("/ig66")) {
     window.location.href = "/ig66";
     return;
@@ -292,7 +291,6 @@ function on_monkey_button_click(e) {
 
 function monkey_button_loader() {
   $("#monkey-button").bind("click", on_monkey_button_click);
-  console.log("MOnkey Button loaded");
 }
 
 $(monkey_button_loader);
