ffc6719e8959d4fac66654b1bf1c52ef71c8790a
Igor Dvorkin <idvorkin@gmail.com>
2023-07-16 10:41:16 -0700

    Updates

--- _d/ai-journal.md
+++ _d/ai-journal.md
@@ -13,6 +13,7 @@ Woah, there's so much stuff to explore in AI. I'm going to keep a journal of stu
     - [Weights and Balances](#weights-and-balances)
 - [Diary](#diary)
     - [2023-07-16](#2023-07-16)
+    - [2023-07-15](#2023-07-15)
     - [2023-07-04](#2023-07-04)
     - [Related posts](#related-posts)
 
@@ -29,15 +30,23 @@ Woah, there's so much stuff to explore in AI. I'm going to keep a journal of stu
 
 - Doing Q&A for my blog w/ChromaDB and LandChain
   - Bleh, Langchain is hard to grok, changing on the daily
+  - <https://github.com/idvorkin/idvorkin.github.io/blob/master/scripts/qa_blog.py?plain=1#L99>
+
+### 2023-07-15
+
+- Had very clever idea to 1/ make my convo files markdown, giving me completion from CoPilot
+- Having GPT prompt default answer in [markdown with line seperators](https://github.com/idvorkin/nlp/blob/ed56c29719a8a8f53ffd10513ec96d3895a25076/convos/default.convo?plain=1#L1) so that it was clear the answers from the bot
+- e.g. <https://gist.github.com/idvorkin/98cfaa4bea8e4f9cc113ff978612518e>
 
 ### 2023-07-04
 
 - Weight and Biases LangChain Traces
+  - <https://docs.wandb.ai/guides/prompts/quickstart>
   - See each call to the model
 
 ```zsh
-LANGCHAIN_WANDBB_TRACING=true
-LANGCHAIN_WANDB_TRACING=playing-around
+export LANGCHAIN_WANDBB_TRACING=true
+export LANGCHAIN_WANDB_TRACING=playing-around
 ```
 
 - Consider a better git commit message generator... Including nested calls. Checkout
--- scripts/qa_blog.py
+++ scripts/qa_blog.py
@@ -3,13 +3,15 @@ import requests
 import pathlib
 import subprocess
 import tempfile
+import openai
 from rich.console import Console
 from icecream import ic
 import typer
+import time
 import os
 from loguru import logger
 from pudb import set_trace
-from langchain.chains import RetrievalQA
+from langchain.chains import ConversationalRetrievalChain
 from langchain.llms import OpenAI
 from langchain.docstore.document import Document
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -17,35 +19,7 @@ from langchain.vectorstores import Chroma
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.prompts import PromptTemplate
 from typing_extensions import Annotated
-
-# ++ openai_wrapper
-text_model_gpt_4 = "gpt-4-0613"
-gpt_4_tokens = 7800
-text_model_gpt35 = "gpt-3.5-turbo-16k-0613"
-gpt_3_5_tokens = 16000
-code_model_best = "code-davinci-003"
-
-
-def model_to_max_tokens(model):
-    model_to_tokens = {text_model_gpt_4: gpt_4_tokens, text_model_gpt35: gpt_3_5_tokens}
-    return model_to_tokens[model]
-
-
-def choose_model(u4, tokens=0):
-    model = "SPECIFY_MODEL"
-    if u4:
-        model = text_model_gpt_4
-    else:
-        model = text_model_gpt35
-
-    is_token_count_the_default = tokens == 0  # TBD if we can do it without hardcoding.
-    if is_token_count_the_default:
-        tokens = model_to_max_tokens(model)
-
-    return model, tokens
-
-
-# -- openai_wrapper
+from openai_wrapper import choose_model, remaining_response_tokens
 
 app = typer.Typer()
 console = Console()
@@ -95,33 +69,124 @@ def build():
     search_index.persist()
 
 
+### Todo, move this to openai_wrapper
+def base_query(
+    tokens: int = 300,
+    debug: bool = False,
+    prompt_to_gpt="replace_prompt",
+    system_prompt="You are a helpful assistant.",
+    u4=False,
+):
+    model, tokens = choose_model(u4, tokens)
+
+    # Define the messages for the chat
+    messages = [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": prompt_to_gpt},
+    ]
+
+    output_tokens = remaining_response_tokens(model, system_prompt, prompt_to_gpt)
+
+    if debug:
+        ic(system_prompt)
+        # ic(prompt_to_gpt)
+        ic(model)
+        ic(output_tokens)
+
+    start = time.time()
+    response_contents = [""]
+    first_chunk = True
+    for chunk in openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        max_tokens=output_tokens,
+        temperature=0.7,
+        stream=True,
+    ):
+        if not "choices" in chunk:
+            continue
+
+        for elem in chunk["choices"]:  # type: ignore
+
+            if first_chunk:
+                if debug:
+                    out = f"First Chunk took: {int((time.time() - start)*1000)} ms"
+                    ic(out)
+                first_chunk = False
+            delta = elem["delta"]
+            delta_content = delta.get("content", "")
+            response_contents[elem["index"]] += delta_content
+
+    text = ""
+    for i, content in enumerate(response_contents):
+        text = content
+        print(text)
+
+    if debug:
+        out = f"All chunks took: {int((time.time() - start)*1000)} ms"
+        ic(out)
+
+
 @app.command()
 def ask(
     question: Annotated[
         str, typer.Argument()
     ] = "What are the roles from Igor's Eulogy, answer in bullet form",
-    u4: bool = typer.Option(True),
-    debug: bool = typer.Option(1),
+    u4: bool = typer.Option(False),
+    debug: bool = typer.Option(True),
 ):
     model = "gpt-4" if u4 else "gpt-3.5-turbo-16k"
     model, max_tokens = choose_model(u4)
     openai = OpenAI(model=model)
+    verbose = False
+    if debug:
+        ic(model)
+        verbose = True
     # load chroma from DB
     blog_content_db = Chroma(
         persist_directory=chroma_db_dir, embedding_function=embeddings
     )
-    qa = RetrievalQA.from_chain_type(
-        llm=OpenAI(),
-        chain_type="map_reduce",
-        retriever=blog_content_db.as_retriever(),
-        return_source_documents=True,
-    )
-    # answer = qa.run(question)
-    answer = qa({"query": question})
-    print(f"Answer: {answer['result']}")
-    print("Sources:")
-    for doc in answer["source_documents"]:
-        print(doc.metadata["source"])
+    history = []
+    facts = blog_content_db.similarity_search(question, max_results=5)
+    # set_trace()
+    # set_trace()
+    for f in facts:
+        ic(f.metadata["source"])
+
+    facts_for_prompt = ""
+    for i, f in enumerate(facts):
+        facts_for_prompt += f"""---(Fact {i})---
+SOURCE:
+    {f.metadata["source"]}
+FACT:
+    {f.page_content}
+---
+"""
+
+    prompt = f"""
+    Use the following document snippets to answer the following questions. After you answer, return the list of sources that you used to answer the question in a markdown list.
+    :
+    ### Facts:
+{facts_for_prompt}
+    ### Question
+        {question}
+    """
+    resp = base_query(prompt_to_gpt=prompt, debug=debug, u4=u4)
+    ic(resp)
+    # ic(prompt)
+
+    return """
+        qa = ConversationalRetrievalChain.from_llm(llm=openai, chain_type="map_reduce", retriever=blog_content_db.as_retriever(), return_source_documents=True, verbose=verbose, )
+        # answer = qa.run(question)
+        answer = qa({"question": question,
+                    "chat_history":history,
+                    })
+        print (f"Answer: {answer['result']}")
+        print ("Sources:")
+        for doc in answer["source_documents"]:
+            print(doc.metadata["source"])
+        ic(history)
+    """
 
 
 # @logger.catch()
