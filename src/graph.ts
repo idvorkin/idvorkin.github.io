feb1f6191b1b4cc950c61bb90a1b329af64d4155
Igor Dvorkin <idvorkin@gmail.com>
2024-06-29 13:27:06 -0700

    Add graph

---
+++ src/graph.ts
@@ -0,0 +1,19 @@
+//
+//  main.js
+//
+// Random tree
+import ForceGraph from "force-graph";
+const N = 300;
+const gData = {
+  nodes: [...Array(N).keys()].map(i => ({ id: i })),
+  links: [...Array(N).keys()]
+    .filter(id => id)
+    .map(id => ({
+      source: id,
+      target: Math.round(Math.random() * (id - 1)),
+    })),
+};
+
+const Graph = ForceGraph()(document.getElementById("graph"))
+  .linkDirectionalParticles(2)
+  .graphData(gData);
